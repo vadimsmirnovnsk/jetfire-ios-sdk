c3b03cf279f408b3c1d1d3a2fc60045cd1c02eea
Смирнов Вадим Павлович <va.smirnov@2gis.ru>
2021-10-12 00:54:37 +0300

    Fix analytics

--- Sources/jetfire-sdk/BaseStories/StoriesConfig.swift
+++ Sources/jetfire-sdk/BaseStories/StoriesConfig.swift
@@ -50,38 +50,38 @@ internal class StoriesAnalytics: IStoriesAnalytics {
 
 	// Firebase Story and Featuring
 	func trackStorySnapDidShow(storyId: String, index: Int) {
-//		Anl.track {
-//			$0.name(.jetfire_story_snap_show)
-//				.param(.jetfire_featuring_id, value: storyId)
-//				.param(.jetfire_snap_index, value: index)
-//		}
+		Anl.track {
+			$0.name(.jetfire_story_snap_show)
+				.param(.jetfire_featuring_id, value: storyId)
+				.param(.jetfire_snap_index, value: index)
+		}
 
 		self.externalAnalytics?.trackStorySnapDidShow(storyId: storyId, index: index)
 	}
 
 	func trackStoryDidStartShow(storyId: String) {
-//		Anl.track {
-//			$0.name(.jetfire_story_start_show)
-//				.param(.jetfire_featuring_id, value: storyId)
-//		}
+		Anl.track {
+			$0.name(.jetfire_story_start_show)
+				.param(.jetfire_featuring_id, value: storyId)
+		}
 
 		self.externalAnalytics?.trackStoryDidStartShow(storyId: storyId)
 	}
 
 	func trackStoryDidFinishShow(storyId: String) {
-//		Anl.track {
-//			$0.name(.jetfire_story_finish_show)
-//				.param(.jetfire_featuring_id, value: storyId)
-//		}
+		Anl.track {
+			$0.name(.jetfire_story_finish_show)
+				.param(.jetfire_featuring_id, value: storyId)
+		}
 
 		self.externalAnalytics?.trackStoryDidFinishShow(storyId: storyId)
 	}
 
 	func trackStoryDidTapButton(buttonOrSnapId: String, buttonTitle: String) {
-//		Anl.track {
-//			$0.name(.jetfire_story_cta_tap)
-//				.param(.jetfire_featuring_id, value: buttonOrSnapId)
-//		}
+		Anl.track {
+			$0.name(.jetfire_story_cta_tap)
+				.param(.jetfire_featuring_id, value: buttonOrSnapId)
+		}
 
 		self.externalAnalytics?.trackStoryDidTapButton(buttonOrSnapId: buttonOrSnapId, buttonTitle: buttonTitle)
 	}
