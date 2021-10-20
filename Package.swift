d725cb698b6d3d8bf3e9672259bf76b6f517d561
Смирнов Вадим Павлович <va.smirnov@2gis.ru>
2021-10-20 20:38:51 +0300

    Update sdwebimage

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/teanet/VNBase.git", .branch("master")),
 		.package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: "5.4.3"),
-		.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+		.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
 		.package(url: "https://github.com/yeahdongcn/UIColor-Hex-Swift", from: "5.1.7"),
 	],
     targets: [
