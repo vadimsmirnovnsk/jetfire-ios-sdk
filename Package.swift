ca089f6fc0ac4c9d364665b34ca06dc4f642f15b
Alexander Volokhin <volokhin@bk.ru>
2022-02-18 13:20:23 +0700

    Integration fixes 2

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 	dependencies: [
 		.package(url: "https://github.com/teanet/VNBase.git", .branch("master")),
-		.package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire", from: "5.4.3"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.4")),
 		.package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
         .package(name: "UIColorHexSwift", url: "https://github.com/yeahdongcn/UIColor-Hex-Swift", from: "5.1.7"),
 		.package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.17.0"),
