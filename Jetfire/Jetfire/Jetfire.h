3c4bb7cc0bb18a7357e49323bda178706cd28b1b
Смирнов Вадим Павлович <va.smirnov@2gis.ru>
2022-04-03 20:03:16 +0700

    Ветка для сборки бинарного фреймворка в папке Jetfire

---
+++ Jetfire/Jetfire/Jetfire.h
@@ -0,0 +1,18 @@
+//
+//  Jetfire.h
+//  Jetfire
+//
+//  Created by Смирнов Вадим Павлович on 03.04.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Jetfire.
+FOUNDATION_EXPORT double JetfireVersionNumber;
+
+//! Project version string for Jetfire.
+FOUNDATION_EXPORT const unsigned char JetfireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jetfire/PublicHeader.h>
+
+
